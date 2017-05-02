2df8f2c59d2c8fb6ccd96e5b051fdf0652312161
Joel Tannas <jtannas@gmail.com>
2017-05-02 23:32:46 +0000

    Added config file

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
+"""
+Simple Flask application.
+"""
+
+### IMPORTS ###################################################################
+import os
 from flask import Flask
+
+### APPLICATION ###############################################################
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+print(os.environ['APP_SETTINGS'])
 
 
+### VIEWS #####################################################################
 @app.route('/')
 def hello():
     return "Hello World!"
 
+
 @app.route('/<name>')
 def hello_name(name):
-    return f"Hello {name}!"
+    return "Hello {}!".format(name)
 
+
+### RUN MAIN ##################################################################
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)

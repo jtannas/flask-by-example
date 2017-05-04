47f3c264e0535b3ccaa7079cc99d60612d6d76c5
Joel Tannas <jtannas@gmail.com>
2017-05-04 07:13:05 +0000

    completed part 3

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ Simple Flask application.
 """
 
 ### IMPORTS ###################################################################
+import operator
 import os
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+import requests
+from collections import Counter
+from flask import Flask, render_template, request
 from models import db, Result
+
+from text_processing import get_request_natural_language
 
 ### APPLICATION ###############################################################
 app = Flask(__name__)
@@ -16,14 +20,52 @@ db.init_app(app)
 
 
 ### VIEWS #####################################################################
-@app.route('/')
-def hello():
-    return "Hello World!"
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    """
+    Index Page. Serves and processes a form that gets a URL from the
+    user.
+    """
+    errors = []
+    html_results = {}
+    if request.method == 'POST':
+        try:
+            url = request.form['url']
+            r = requests.get(url)
+        except:
+            errors.append(
+                'Unable to get URL. Please make sure it is valid and try again.'
+            )
+            return render_template('index.html', errors=errors)
 
+        if r is not None:
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
+            ### Get the Natural Language ###
+            word_lists = get_request_natural_language(r)
+
+            ### Perform the word counts ###
+            raw_words_count = Counter(word_lists['raw'])
+            no_stop_words_count = Counter(word_lists['no_stops'])
+
+            ### Prepare the results ###
+            html_results = sorted(
+                no_stop_words_count.items(),
+                key=operator.itemgetter(1),
+                reverse=True, )[:10]
+
+            db_result = Result(
+                url=url,
+                result_all=raw_words_count,
+                result_no_stop_words=no_stop_words_count)
+
+            ### Save the results ###
+            try:
+                db.session.add(db_result)
+                db.session.commit()
+            except:
+                errors.append("Unable to add item to database.")
+
+    return render_template('index.html', errors=errors, results=html_results)
 
 
 ### RUN MAIN ##################################################################
--- manage.py
+++ manage.py
@@ -1,15 +1,28 @@
+"""
+This module is part of implementing Flask-Script.
+
+The Flask-Script extension provides support for writing external
+scripts in Flask. This includes running a development server, a
+customised Python shell, scripts to set up your database, cronjobs, and
+other command-line tasks that belong outside the web application itself.
+"""
+### IMPORTS ###################################################################
 import os
-from flask_script import Manager
+from flask_script import Server, Manager
 from flask_migrate import Migrate, MigrateCommand
 
 from app import app, db
 
+### CONFIG THE APP AND REGISTER EXTENSIONS ####################################
 app.config.from_object(os.environ['APP_SETTINGS'])
 
 migrate = Migrate(app, db)
 manager = Manager(app)
 
+### COMMANDS ##################################################################
 manager.add_command('db', MigrateCommand)
+manager.add_command('runserver', Server(host='0.0.0.0', port=8080))
 
+### RUN MAIN ##################################################################
 if __name__ == '__main__':
     manager.run()
